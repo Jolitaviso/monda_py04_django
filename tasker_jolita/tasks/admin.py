097450fddbf6c36903b5865eb7c56ef6df3a6e82
Jolitaviso <jolitavinter@gmail.com>
2024-02-13 11:34:08 +0200

    admin

--- tasker_jolita/tasks/admin.py
+++ tasker_jolita/tasks/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from . import models
 
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'owner']
+    list_display_links = ['name']
+    list_filter = ['owner']
+    
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_done', 'deadline', 'project', 'owner', 'created_at']
+    list_filter = ['is_done', 'project', 'owner', 'deadline', 'created_at']
+
 admin.site.register(models.Project)
 admin.site.register(models.Task)
