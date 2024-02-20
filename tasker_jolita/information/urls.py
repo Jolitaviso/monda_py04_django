ecad001dfdf1d9ae9af93cfca2c0ce954316f69a
Jolitaviso <jolitavinter@gmail.com>
2024-02-20 11:22:18 +0200

    Pataisyta viršuje kur rodo Miau |

--- tasker_jolita/information/urls.py
+++ tasker_jolita/information/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('information/', views.information_list, name='information_list'),
-    path('information/<int:pk>/', views.information_detail, name='information_detail'),
+    path('', views.information_list, name='information_list'),
+    path('<int:pk>/', views.information_detail, name='information_detail'),
 ]
